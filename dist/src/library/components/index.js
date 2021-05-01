2a470a539e1f845d78475a23f839ffd2a94c7024
Appasaheb Lakade <onlyappasaheb4@gmail.com>
2021-05-01 12:34:01 +0530

    remove same packages

--- dist/src/library/components/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Molecules = void 0;
-const Molecules = require("./molecules");
-exports.Molecules = Molecules;
--- dist/src/library/components/molecules/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AutocompleteGroupBy = void 0;
-var AutocompleteGroupBy_1 = require("./AutocompleteGroupBy");
-Object.defineProperty(exports, "AutocompleteGroupBy", { enumerable: true, get: function () { return AutocompleteGroupBy_1.default; } });
