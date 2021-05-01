a94645af7b7eddb851b76b43ec07ed715c9f6d2b
Appasaheb Lakade <onlyappasaheb4@gmail.com>
2021-05-01 16:40:52 +0530

    lib modify

---
+++ lib/cjs/molecules/AutocompleteGroupBy.d.ts
@@ -0,0 +1,7 @@
+export interface AutocompleteGroupByProps {
+    data?: any[];
+    onChange?: () => void;
+    onClose?: () => void;
+}
+declare const AutocompleteGroupBy: (props: AutocompleteGroupByProps) => any;
+export default AutocompleteGroupBy;
